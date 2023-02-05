5d3248a100be9c4f23323705be406a53c0a01351
nightrally <nightrallymain@gmail.com>
2023-02-05 21:07:05 +0000

    New demiboss + Corrupt Pact improvements

--- fleshwerk/lua/autorun/client/cl_demiboss.lua
+++ fleshwerk/lua/autorun/client/cl_demiboss.lua
@@ -64,7 +64,7 @@ function ZCorruptionPact()
         local costlbl = vgui.Create("DLabel", pnl)
         costlbl:SetText("CC$"..tostring(cost))
         costlbl:SetFont("ZSHUDFont")
-        costlbl:SetTextColor(NZSCOLOR_CREDITS)
+        costlbl:SetTextColor(lp:GetNWInt("ZCorruptCreds") < cost and NZSCOLOR_RED or NZSCOLOR_CREDITS)
         costlbl:SizeToContents()
         costlbl:Dock(RIGHT)
         costlbl:DockMargin(0, 0, 16, 0)
@@ -72,6 +72,7 @@ function ZCorruptionPact()
         local namelbl = vgui.Create("DLabel", pnl)
         namelbl:SetText(name)
         namelbl:SetFont("ZSHUDFontSmall")
+        namelbl:SetTextColor(lp:GetNWInt("ZCorruptCreds") < cost and NZSCOLOR_RED or color_white)
         namelbl:SizeToContents()
         namelbl:Dock(FILL)
         namelbl:SetContentAlignment(5)
@@ -82,14 +83,14 @@ function ZCorruptionPact()
         btn:SetTooltip(desc)
         btn.Paint = function() end
         btn.DoClick = function()
-            if lp:GetNWInt("ZCorruptCreds") < cost or GAMEMODE:GetWave() == 0 or not GAMEMODE:GetWaveActive() or lp:GetZombieClassTable().Demiboss or lp:GetZombieClassTable().Boss then
+            if lp:GetNWInt("ZCorruptCreds") < cost or GAMEMODE:GetWave() == 0 or not GAMEMODE:GetWaveActive() or lp:GetZombieClassTable().Demiboss or lp:GetZombieClassTable().Boss or lp:GetZombieClassTable().GreatEvil then
                 surface.PlaySound("buttons/combine_button_locked.wav")
 
                 if lp:GetNWInt("ZCorruptCreds") < cost then
                     lp:ChatPrint("You don't have enough Corruption Credits!")
                 elseif GAMEMODE:GetWave() == 0 or not GAMEMODE:GetWaveActive() then
                     lp:ChatPrint("You can't purchase anything right now!")
-                elseif lp:GetZombieClassTable().Demiboss or lp:GetZombieClassTable().Boss then
+                elseif lp:GetZombieClassTable().Demiboss or lp:GetZombieClassTable().Boss or lp:GetZombieClassTable().GreatEvil then
                     lp:ChatPrint("You are currently a restricted zombie class!")
                 end
             else
@@ -105,7 +106,8 @@ function ZCorruptionPact()
         end
     end
 
-    Zombie("Flesh Forager", "The Flesh Forager is slow and weak but has the ability to throw Bonemesh bombs.", "nightrally/killicons/zombies/fleshforager", 1500)
+    Zombie("Flesh Forager", "The Flesh Forager is slow and weak but has the ability to throw Bonemesh bombs.\nIt can also heal itself along with other undeads indefinitely.", "nightrally/killicons/zombies/fleshforager", 800)
+    Zombie("Undead Soldier", "The Undead Soldier hasn't lost its ability handling a fully auto assault rifle.\nBecause of this, the Undead Soldier is unable to use any melee attacks.\nIt's best to stay a medium distance away from humans.", "nightrally/killicons/zombies/undeadsoldier", 800)
 end
 
 nightrally.LogFile("cl_demiboss.lua")
