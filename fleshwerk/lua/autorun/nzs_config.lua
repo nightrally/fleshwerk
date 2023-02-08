ce597babf390811f8e9529ae3200f13266a3e4f8
nightrally <nightrallymain@gmail.com>
2023-02-08 21:12:55 +0000

    Increase max level + update tip messages

--- fleshwerk/lua/autorun/nzs_config.lua
+++ fleshwerk/lua/autorun/nzs_config.lua
@@ -9,22 +9,21 @@ nightrally.StartingLoadouts = {
 
 -- 0.75x Default knockback for non-listed maps
 nightrally.BoomstickKnockbackMul = {
-	["gm_construct"] = 0.5,
-	["zs_obj_devourer_towerz"] = 1
+	["gm_construct"] = 0.5
 }
 
-nightrally.MaxLevel = 50
+nightrally.MaxLevel = 80
 nightrally.XPMul = 1
-nightrally.nightrallyXPMul = 2
+nightrally.nightrallyXPMul = 10
 
 nightrally.ZombieHealthMul = 1
 nightrally.ZombieSpeedMul = 1
 nightrally.BossHealthMul = 1
 nightrally.BossSpeedMul = 1
 
-nightrally.PropHealthMul = 0.85 * 1.5
-nightrally.PropRepairMul = 1.5
-nightrally.ExtraHealthPerExtraNailMul = 0.85 * 1.5
+nightrally.PropHealthMul = 1
+nightrally.PropRepairMul = 1
+nightrally.ExtraHealthPerExtraNailMul = 1
 
 nightrally.PlayersForBoss = 0
 
@@ -32,14 +31,16 @@ nightrally.ChatTips = {
 	"You need 38,416 total XP to remort.",
 	"You can view your stats by typing !stats.",
 	"Your stats (!stats) are saved permanently even between map changes and upon disconnecting. So rack up those stats!",
-	"You can open the nZS options menu by typing !nzsoptions.",
-	"You can open the nZS credits menu by typing !nzscredits.",
+	"You can open the nZS options menu by typing !options.",
+	"You can open the nZS credits menu by typing !credits.",
 	"You can access the zombie class selection menu by pressing F3.",
 	"You can access the Worth Menu or Points Shop by pressing F2.",
-	"You can disable nZS Tips by typing !nzsoptions.",
+	"You can disable nZS Tips by typing !options.",
 	"We have punishments such as chat mutes, voice mutes, aegis bans, hammer bans, etc. So make sure not to break our rules!",
 	"Staff members are able to spawn nests with commands if it's not used in an abusive way.",
-	"Want to join our Discord server? Type !discord to join our community."
+	"Want to join our Discord server? Type !discord to join our community.",
+	"You can open the nZS staff list menu by typing !staff.",
+	"As a zombie, you gain Corruption Credits which you can spend on items in Corruption Pact. Just press F2 to open it!"
 }
 
 nightrally.TotalSkillTrees = 7
