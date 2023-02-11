29bb2e2ca04c4d554b3e05e2ae688be6a17f4c43
nightrally <nightrallymain@gmail.com>
2023-02-11 11:37:28 +0000

    config tweaks

--- fleshwerk/lua/autorun/nzs_config.lua
+++ fleshwerk/lua/autorun/nzs_config.lua
@@ -4,20 +4,7 @@ nightrally.TimeLimit = 15
 nightrally.RoundLimit = 3
 nightrally.MaxSigils = 3
 nightrally.StartingLoadouts = {
-	{"blstr", "2sgcp", "3sgcp", "medkit", "60mkit", "90mkit"},
-	{"pshtr", "resupplybox", "3pcp", "2pcp"},
-	{"btlax", "3pcp", "2pcp", "remantler"},
-	{"stbbr", "3rcp", "2rcp", "targetingvisi"},
-	{"tossr", "3smgcp", "medkit"},
-	{"blstr", "3sgcp", "2sgcp", "infturret"},
-	{"owens", "3pcp", "2pcp", "2smgcp", "3smgcp"},
-	{"remantler", "medkit", "90mkit", "60mkit"},
-	{"minelayer", "4mines", "6mines", "pipe"},
-	{"crklr", "3arcp", "2arcp", "csknf", "stone"},
-	{"junkpack", "crphmr", "barricadekit"},
-	{"z9000", "3pls", "2pls", "medkit"},
-	{"zpaxe", "60mkit", "medkit"},
-	{"drone", "3smgcp", "2smgcp", "wrench"}
+-- Fuck starting loadouts for now, maybe give random t1s + ammo or something?
 }
 
 -- 0.75x Default knockback for non-listed maps
@@ -38,7 +25,7 @@ nightrally.PropHealthMul = 1
 nightrally.PropRepairMul = 1
 nightrally.ExtraHealthPerExtraNailMul = 1
 
-nightrally.PlayersForBoss = 8
+nightrally.PlayersForBoss = 16
 
 nightrally.ChatTips = {
 	"You can view your stats by typing !stats.",
@@ -48,39 +35,11 @@ nightrally.ChatTips = {
 	"You can access the zombie class selection menu by pressing F3.",
 	"You can access the Worth Menu or Points Shop by pressing F2.",
 	"You can disable nZS Tips by typing !options.",
-	"Make sure not to break our rules.",
-	"Want to join our Discord server? Type !discord to join our community.",
+	"Want to join our Discord server? Type !discord.",
 	"You can open the nZS staff list menu by typing !staff.",
 	"As a zombie, you gain Corruption Credits which you can spend on items in Corruption Pact. Press F2 to open it.",
-	"Press UNDO or ZOOM as a human to phase through barricades.",
-	"Press SHIFT as most zombie classes to feign death.",
-	"Press SHIFT as a headcrab to burrow under the ground.",
-	"Use your flashlight as a zombie to toggle zombie vision.",
-	"The fear-o-meter at the bottom of the screen is a measure of how many zombies are near you.",
-	"Zombies take reduced damage as the fear-o-meter increases.",
-	"New player? Try letting more experienced players create barricades for a few games to learn.",
-	"Arsenal Crate owners receive a percentage of the points people use to buy weapons and items.",
-	"Use the medical kit to heal yourself and others and cure poison. Heal others to receive points.",
-	"Press F3 as a zombie to change classes.",
-	"Press RELOAD to pry out nails with your hammer. Prying out another person's nails will give you penalties.",
-	"Pack up deployed items by pressing SHIFT on them.",
-	"You can remove deployables placed by other people by pressing SHIFT on them with a few other people.",
-	"Press ALT to check your ammo, drop weapons, and drop ammo.",
-	"Change game options by pressing F4.",
-	"A boss zombie is selected every wave intermission. The most aggressive zombie player gets picked.",
-	"As a zombie, you can select which boss zombie you want to be chosen as in the F3 menu.",
-	"Press USE on resupply crates to get free ammo every so often.",
-	"Don't stand too close to a barricade to avoid getting hit.",
-	"Zombies can be slowed for a short time by shooting them in the legs.",
-	"More isn't always better. Leave small areas open in your barricades to ensure shooting space.",
-	"Headcrabs can easily enter windows and small openings in barricades.",
-	"Win the round to receive an xp bonus.",
-	"Ghouls attacks debuff humans causing other attacks to do more damage.",
-	"Don't say stupid shit. Seriously.",
-	"If you find a bug, don't abuse it. Just report it.",
-	"Admins have an icon in tab next to their name. Anyone else is an imposter!",
-	"Press F1 for help.",
-	"You can play music through your microphone, but dont abuse it."
+	"Ghouls give enfeeble on hit which makes you more vulnerable to all damage sources!",
+	"The max level is currently level "..nightrally.MaxLevel.."."
 }
 
 nightrally.TotalSkillTrees = 7
