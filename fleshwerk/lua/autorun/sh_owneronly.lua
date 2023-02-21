4a607e075dc21ec80ad3ddcf3e009465dab6b232
nightrally <nightrallymain@gmail.com>
2023-02-21 21:45:49 +0000

    fix

--- fleshwerk/lua/autorun/sh_owneronly.lua
+++ fleshwerk/lua/autorun/sh_owneronly.lua
@@ -3,10 +3,7 @@ if SERVER then
         if pl:Team() == TEAM_HUMAN then
             if pl:nightrally()then
                 pl:Give("weapon_zs_tommygun")
-                pl:SetModel("models/player/fnaf_sexy/frederika.mdl")
-            elseif pl:Anakinhall06() then
-                pl:Give("weapon_zs_tommygun")
-                pl:SetModel("models/kyo/ghost_azrael_pm.mdl")
+                pl:SetModel("models/player/reaper.mdl")
             end
         end
     end)
